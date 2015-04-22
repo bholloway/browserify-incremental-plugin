f0bcefa4dc28f624ff1a7878095b8ff748bc209f
benholloway <ben.holloway@3plearning.com>
2015-04-22 12:23:52 +1000

    minor fixes

--- index.js
+++ index.js
@@ -1,53 +1,94 @@
 'use strict';
 
-var through = require('through2');
+var fs = require('fs');
+
+var through = require('through2'),
+    merge   = require('lodash.merge');
 
 /**
- * A browserify plugin that anonymises filename labels in the browser-pack.
- * @param {object} bundler The browserify bundler instance
- * @param {object} opt An options hash
+ * Create a context for which multiple instances of the plugin may operate with a shared cache.
+ * @param {object} [cache] Optional cache
+ * @returns {function} A browserify plugin
  */
-function browserifyAnonymousLabeler(bundler, opt) {
-  if (bundler && (typeof bundler === 'object') && ('on' in pipeline) && ('bundler' in pipeline)) {
-    bundler.on('reset', setupPipeline());
-    setupPipeline();
-  }
-  else {
-    throw new Error('Expected a browserify bundler instance')
-  }
+function pluginFactory(cache) {
+
+  // ensure cache
+  var internalCache = cache || {};
+
+  // return a closure with a getContext() sidecar
+  browserifyIncremental.pluginFactory = pluginFactory;
+  return browserifyIncremental;
 
   /**
-   * Apply the labeler to the pipeline.
+   * A browserify plugin that checks incoming file context and uses cached results.
    * @param {object} bundler The browserify bundler instance
+   * @param {object} opt An options hash
    */
-  function setupPipeline() {
-    bundler.pipeline
-      .get('label')
-      .push(anonymousLabeler());
+  function browserifyIncremental(bundler, opt) {
+  var isValid = bundler && (typeof bundler === 'object') &&
+    (typeof bundler.on === 'function') && (typeof bundler.pipeline === 'object');
+    if (isValid) {
+      bundler.on('reset', setupPipeline);
+      setupPipeline();
+    }
+    else {
+      throw new Error('Expected a browserify bundler instance')
+    }
+
+    /**
+     * Apply an interceptor to the pipeline.
+     * @param {object} bundler The browserify bundler instance
+     */
+    function setupPipeline() {
+      var deps = bundler.pipeline.get('deps');
+      deps.push(populateCache(internalCache, deps._streams[0].cache));
+    }
   }
 }
 
-module.exports = browserifyAnonymousLabeler;
+module.exports = pluginFactory();
 
 /**
- * A pipeline labeler that ensures that final file names are anonymous in the final output
- * @returns {stream.Through} A through stream for the labelling stage
+ * A pipeline 'deps' stage that populates cache for incremental compile.
+ * Called on fully transformed row but only when there is no cache hit.
+ * @param {object} internalCache Our internal cache
+ * @param {object} depsCache The cache used by module-deps
+ * @returns {stream.Through} a through stream
  */
-function anonymousLabeler() {
+function populateCache(internalCache, depsCache) {
   function transform(row, encoding, done) {
     /* jshint validthis:true */
-    Object.keys(row.deps)
-      .forEach(function eachDep(key) {
-        var value = row.deps[key];
-        row.deps[String(value)] = value;
-        row.source = row.source
-          .split(key)
-          .join(value);
-        delete row.deps[key];
+    var filename = row.file;
+
+    // set the new transformed row output
+    internalCache[filename] = {
+      input : fs.readFileSync(filename).toString(),
+      output: {
+        id    : filename,
+        source: row.source,
+        deps  : merge({}, row.deps),
+        file  : filename
+      }
+    };
+
+    // we need to use a getter as it is the only hook at which we can perform comparison
+    //  getters cannot be redefined so we create on first access and retain, hence the need
+    //  for the internal cache to store the value above
+    if (!depsCache.hasOwnProperty(filename)) {
+      Object.defineProperty(depsCache, filename, {
+        get: function () {
+          // file read and comparison is in the order of 100us
+          var cached  = internalCache[filename];
+          var input   = fs.readFileSync(filename).toString();
+          var isMatch = (cached.input === input);
+          return isMatch ? cached.output : undefined;
+        }
       });
+    }
+
+    // complete
     this.push(row);
     done();
   }
-
   return through.obj(transform);
 }
